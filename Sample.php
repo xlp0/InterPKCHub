12dba3fcd1a7e3bb37b517cab5886da798c34d8f
Ben Koo <koo0905@gmail.com>
2021-05-06 12:12:10 +0800

    Fixed some code

--- Sample.php
+++ Sample.php
@@ -3,7 +3,7 @@
 
 
 ## The protocol and server name to use in fully-qualified URLs
-$wgServer = "http://localhost:9352";
+$wgServer = "http://thewiki.us:9352";
 
 # The following two lines contains information on Github's OAuth service. You will have to apply for your own information to get things to work.
 $wgOAuth2Client['client']['id'] = "83698ede718fea93a79e";
@@ -12,7 +12,7 @@ $wgOAuth2Client['client']['secret'] = "290648a66406e1bb3c5655a96c34b62fac5e4e9a"
 $wgOAuth2Client['configuration']['authorize_endpoint']     = 'https://github.com/login/oauth/authorize'; // Authorization URL
 $wgOAuth2Client['configuration']['access_token_endpoint']  = 'https://github.com/login/oauth/access_token'; // Token URL
 $wgOAuth2Client['configuration']['api_endpoint']           = 'https://api.github.com/user'; // URL to fetch user JSON
-$wgOAuth2Client['configuration']['redirect_uri'] = "http://localhost:9352/index.php/Special:OAuth2Client/callback";
+$wgOAuth2Client['configuration']['redirect_uri'] = "http://thewiki.us:9352/index.php/Special:OAuth2Client/callback";
 $wgOAuth2Client['configuration']['username'] = 'login'; // JSON path to username
 $wgOAuth2Client['configuration']['email'] = 'email'; // JSON path to email
 $wgOAuth2Client['configuration']['scopes'] = 'openid email profile'; //Permissions
--- stringReplace.sh
+++ stringReplace.sh
@@ -30,3 +30,50 @@ if [[ $OSTYPE  == *"msys"* ]]; then
 else
     echo "Some other machine $OSTYPE" 
 fi
+
+
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+    # For instance, will be example_kaggle_key
+    echo "Loaded environmental variable: TRANSPORT_STRING=$TRANSPORT_STRING"
+    echo "Loaded environmental variable: HOST_STRING=$HOST_STRING"
+    echo "Loaded environmental variable: PortNumber=$PortNumber"
+    echo "Loaded environmental variable: OAUTH_CLIENT_ID=$OAUTH_CLIENT_ID"
+    #Secret will not show
+    echo "Loaded environmental variable: OAUTH_CLIENT_SECRET=********"
+
+    if [[ ${TRANSPORT_STRING} =~  .*https.* ]]; then
+      echo "To use the following transport string:  ${TRANSPORT_STRING}://$HOST_STRING"
+      replaceString="$HOST_STRING";
+    else
+      echo "To use the following transport string:  ${TRANSPORT_STRING}://$HOST_STRING:$PortNumber"
+      replaceString="$HOST_STRING:$PortNumber";
+    fi
+
+
+    # Localhost configuration based on .env
+    # Substitute to correct config if founded "$TargetKey =" in LocalSetting.php
+    # Only replace 'var' instead of "var" 
+    filename="Sample.php"
+    # Put in all the params for configuration
+    oauth_key_array=(
+      "wgServer" 
+      "wgOAuth2Client\[\'client\'\]\[\'id\'\]" 
+      "wgOAuth2Client\[\'client\'\]\[\'secret\'\]"
+      "wgOAuth2Client\[\'configuration\'\]\[\'redirect_uri\'\]"
+      )
+
+    oauth_val_array=(
+      $TRANSPORT_STRING://${replaceString} 
+      $OAUTH_CLIENT_ID 
+      $OAUTH_CLIENT_SECRET
+      "$TRANSPORT_STRING://${replaceString}/index.php/Special:OAuth2Client/callback"
+      )
+    len=${#oauth_key_array[@]}
+    for (( i=0; i<$len; i++ ));
+    do
+      echo "Replacing string in LocalSettings.php: ${oauth_key_array[$i]}"
+        sed "s|\$${oauth_key_array[$i]}[[:blank:]]*=.*|\$${oauth_key_array[$i]} = \"${oauth_val_array[$i]}\";|" $filename > temp.txt && mv temp.txt $filename
+    done
+fi
